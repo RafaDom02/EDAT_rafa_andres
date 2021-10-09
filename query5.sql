b38ecb29544b5362c747e8ab1141c79a196f722e
Depool0810 <andres.depool@estudiante.uam.es>
2021-10-09 20:02:55 +0200

    queries 5 y 6

---
+++ query5.sql
@@ -0,0 +1,45 @@
+with bookref_to_ticket_no(book_ref, ticket_no) as 
+	(
+	select
+		  b.book_ref, t.ticket_no
+	from
+		bookings b , tickets t
+	where
+		b.book_ref = t.book_ref 
+	group by 
+		b.book_ref, t.ticket_no
+	),
+	
+	ticket_no_to_flight_id(ticket_no, flight_id) as
+	(
+		select 
+			t.ticket_no, tf.flight_id
+		from 
+			tickets t, ticket_flights tf 
+		where
+			t.ticket_no = tf.ticket_no
+		group by 
+			t.ticket_no, tf.flight_id 
+	),
+	
+	book_ref_with_tickets(book_ref, flight_id, ticket_no) as
+	(
+		select
+			  distinct bft.book_ref, tnd.flight_id, tnd.ticket_no
+		from 
+			bookref_to_ticket_no bft natural join ticket_no_to_flight_id tnd
+		where
+			bft.ticket_no = tnd.ticket_no
+	)
+	
+
+select 
+	distinct brwt.book_ref, brwt.flight_id
+from
+	book_ref_with_tickets brwt left join boarding_passes bp 
+on
+	bp.ticket_no = brwt.ticket_no
+where 
+	bp.ticket_no is null
+order by 
+	brwt.book_ref asc, brwt.flight_id asc;
---
+++ query6.sql
@@ -0,0 +1,51 @@
+WITH flight_no_and_id(flight_no, actual_arrival, scheduled_arrival) as
+(
+	SELECT
+		f.flight_no, f.actual_arrival, f.scheduled_arrival
+	FROM
+		flights f
+	group by
+		f.flight_no, f.actual_arrival, f.scheduled_arrival
+),
+flight_delays(flight_no, retraso) as
+(
+	SELECT
+		fni.flight_no, (fni.actual_arrival - fni.scheduled_arrival) as retraso
+	FROM
+		flight_no_and_id fni
+	order by
+		fni.flight_no asc
+),
+flight_average_delay(flight_no, retraso_medio) as
+(
+	SELECT
+		fd1.flight_no, AVG(fd1.retraso) as retraso_medio
+	FROM
+		flight_delays fd1
+	GROUP BY
+		fd1.flight_no
+),
+flight_average_delay_max(flight_no, retraso_medio_max) as
+(
+	SELECT
+		fd.flight_no, fd.retraso_medio as retraso_medio_max
+	FROM
+		flight_average_delay fd
+	GROUP BY
+		fd.flight_no, fd.retraso_medio
+	ORDER BY
+		fd.retraso_medio desc limit 1
+),
+all_flights_with_max_delay as
+(
+	SELECT
+		fad.flight_no, fad.retraso_medio
+	FROM
+		flight_average_delay fad, flight_average_delay_max fadm
+	WHERE
+		fad.retraso_medio = fadm.retraso_medio_max
+)
+
+
+
+SELECT * FROM all_flights_with_max_delay;
