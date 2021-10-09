84871cc453ff63b7429a72375dd49a93b644b6db
RafaDom02 <rafadomsae@outlook.es>
2021-10-09 23:25:45 +0200

    Modificado query2 para que sea un poco mas rapido

--- query2.sql
+++ query2.sql
@@ -1,10 +1,10 @@
 --EJERCICIO 2
 
 select
-	distinct b.book_ref, b.total_amount, sum(tf.amount) 
+	distinct b.book_ref, b.total_amount, sum(tf.amount) as total_amount_calculated
 from
-	bookings b natural join tickets t
-	natural join ticket_flights tf
+	bookings b join tickets t on b.book_ref=t.book_ref
+	join ticket_flights tf on t.ticket_no=tf.ticket_no
 group by
 	b.book_ref
 order by
