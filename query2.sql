0d6057ce3abc537b909a183d4378736cbe83bd9f
RafaDom02 <rafadomsae@outlook.es>
2021-10-12 18:31:59 +0200

    quitado un distinct en la query2 que no tenia mucho sentido

--- query2.sql
+++ query2.sql
@@ -1,7 +1,7 @@
 --EJERCICIO 2
 
 select
-	distinct b.book_ref, b.total_amount, sum(tf.amount) as total_amount_calculated
+	b.book_ref, b.total_amount, sum(tf.amount) as total_amount_calculated
 from
 	bookings b join tickets t on b.book_ref=t.book_ref
 	join ticket_flights tf on t.ticket_no=tf.ticket_no
